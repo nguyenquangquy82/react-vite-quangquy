d358c3310dd9b58e418d50b67c591e33caba7e14
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-02-26 23:03:20 +0700

    video 31

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,8 +1,13 @@
 import './style.css'
 const MyComponent = () =>{
+  // const quangquyit {
+  //   name: "cu em",
+  //   age :22
+  // };
   return (
     <>
-        <div>nguyenquangquy update</div>
+        <div> nguyenquangquy update</div>
+        <div>{console.log("CuEM")}</div>
         <div className="child" style={{borderRadius:"10px"}}> cu cu</div>
     </>
    
