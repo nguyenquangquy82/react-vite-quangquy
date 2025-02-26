aa4bab8553e413e039cc5f4455f2e03ac572316d
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-02-26 23:45:03 +0700

    video 33

---
+++ src/components/todo/ToDoData.jsx
@@ -0,0 +1,10 @@
+const ToDoData = () =>{
+  return (
+    <div className="todo-data">
+          <div> Learning React</div>
+          <div>Watching Youtube</div>
+        </div>
+    
+  )
+}
+export default ToDoData
