729961130e9075fa9c4534aaf0b313cd7e0c0f26
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-02-26 22:53:53 +0700

    video 30

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,8 +1,11 @@
+import './style.css'
 const MyComponent = () =>{
   return (
-    <div >
-     nguyenquangquy update
-    </div> //tag
+    <>
+        <div>nguyenquangquy update</div>
+        <div className="child" style={{borderRadius:"10px"}}> cu cu</div>
+    </>
+   
   );
 }
 export default MyComponent;
