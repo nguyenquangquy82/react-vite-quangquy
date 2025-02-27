82887334ba7e87582b9e51f60be20d92d4cb6bf2
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-02-28 00:49:03 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,16 @@ import {
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UsersPage from './pages/user.jsx';
-import ProductsPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
 import './styles/global.css';
 import ToDoApp from './components/todo/ToDoApp.jsx';
+import ErrorPage from './pages/error.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    errorElement: <ErrorPage/>,
     children:[
       {
         index :true,
@@ -26,8 +28,8 @@ const router = createBrowserRouter([
         element: <UsersPage/>
       },
       {
-        path: "/products",
-        element: <ProductsPage/>
+        path: "/books",
+        element: <BookPage/>
       },
       
     ]
