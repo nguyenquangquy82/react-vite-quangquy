dea9eec3e7e85ae940a19344708fa195fd11f407
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-02-28 22:02:11 +0700

    video 66

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,6 +1,15 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const UsersPage= () =>{
   return (
-    <div> Users page</div>
+    
+   
+          <div style={{padding:"20px"}}>
+            <UserForm/>
+            <UserTable/>
+          </div>
+   
   )
 }
 export default UsersPage;
