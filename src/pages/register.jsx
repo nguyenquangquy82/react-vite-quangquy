7173c3c4a0a1b64ed5a23ea135da11939ed7cf50
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-03-08 01:06:54 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,86 @@
+import { Button, Input ,Form} from "antd";
+
 const RegisterPage =() =>{
+  const [form] = Form.useForm();
+  const onFinish = (value) =>{
+    console.log(">>>check value:",value)
+
+  }
+
   return (
-    <div> Register page</div>
+    <Form
+    form={form}
+    layout="vertical"
+    onFinish={onFinish}
+ 
+ 
+    
+  
+    // onFinishFailed={onFinishFailed}
+   
+  >
+ 
+    <div style={{
+      margin:"50px",
+      // display:"flex",
+      // flexDirection:"column",
+   
+    }}>  
+      <Form.Item
+      label="Full Name"
+      name="fullName"
+      // rules={[
+      //   {
+      //     required: true,
+      //     message: 'Please input your username!',
+      //   },
+      // ]}
+    >
+      <Input />
+    </Form.Item>
+    <Form.Item
+      label="Email"
+      name="email"
+      // rules={[
+      //   {
+      //     required: true,
+      //     message: 'Please input your username!',
+      //   },
+      // ]}
+    >
+      <Input />
+    </Form.Item>
+    <Form.Item
+      label="Password "
+      name="passWord"
+      // rules={[
+      //   {
+      //     required: true,
+      //     message: 'Please input your username!',
+      //   },
+      // ]}
+    >
+      <Input.Password />
+    </Form.Item>
+    <Form.Item
+      label="Phone number"
+      name="phone"
+      // rules={[
+      //   {
+      //     required: true,
+      //     message: 'Please input your username!',
+      //   },
+      // ]}
+    >
+      <Input />
+    </Form.Item>
+    {/* <button type="submit">Register</button> */}
+      
+        <div>
+          <Button onClick={()=>form.submit()} type="primary">Register</Button>
+        </div>
+    </div>
+    </Form>
   )
 }
 
