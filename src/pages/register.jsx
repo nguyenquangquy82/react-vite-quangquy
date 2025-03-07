720b3e167e93cf5049ddeeb01a5d24d8549fa997
nguyenquangquy82 <quangquy8222003@gmail.com>
2025-03-08 01:34:12 +0700

    video 90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,30 @@
-import { Button, Input ,Form} from "antd";
+import { Button, Input ,Form, notification} from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage =() =>{
   const [form] = Form.useForm();
-  const onFinish = (value) =>{
+  const navigate= useNavigate()
+  const onFinish = async (value) =>{
     console.log(">>>check value:",value)
+    const res = await registerUserAPI(
+      value.fullName,
+      value.email,
+      value.password,
+      value.phone);
+    if(res.data){
+      notification.success({
+          message: "Register user",
+          description: "Đăng ký user thành công"
+        })
+        navigate("/login");
 
+      }else {
+        notification.error({
+          message: "Register user error",
+          description: JSON.stringify(res.message)
+        })
+      }
   }
 
   return (
@@ -29,48 +49,50 @@ const RegisterPage =() =>{
       <Form.Item
       label="Full Name"
       name="fullName"
-      // rules={[
-      //   {
-      //     required: true,
-      //     message: 'Please input your username!',
-      //   },
-      // ]}
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
     >
       <Input />
     </Form.Item>
     <Form.Item
       label="Email"
       name="email"
-      // rules={[
-      //   {
-      //     required: true,
-      //     message: 'Please input your username!',
-      //   },
-      // ]}
+      rules={[
+        {
+          required: true,
+          message: 'Please input your Email',
+        },
+      ]}
     >
       <Input />
     </Form.Item>
     <Form.Item
       label="Password "
-      name="passWord"
-      // rules={[
-      //   {
-      //     required: true,
-      //     message: 'Please input your username!',
-      //   },
-      // ]}
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
     >
       <Input.Password />
     </Form.Item>
     <Form.Item
       label="Phone number"
       name="phone"
-      // rules={[
-      //   {
-      //     required: true,
-      //     message: 'Please input your username!',
-      //   },
-      // ]}
+      rules={[
+        {
+          required: true,
+        pattern: new RegExp(/\d+/g),
+        message: "Wrong format!"
+
+        },
+      ]}
     >
       <Input />
     </Form.Item>
@@ -78,6 +100,14 @@ const RegisterPage =() =>{
       
         <div>
           <Button onClick={()=>form.submit()} type="primary">Register</Button>
+          <Button onClick={()=>{
+            form.setFieldsValue({
+              email: "quangquyit@gmail.com",
+              fullname : "quangquy"
+            })
+            console.log("CC")
+           
+          }}>Test</Button>
         </div>
     </div>
     </Form>
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -60,8 +60,20 @@ const updateUserAvatarAPI = (avatar,_id,fullName,phone) =>{
   
 }
 
+const registerUserAPI = (fullName,email,password,phone) =>{
+  const URL_BACKEND= "/api/v1/user/register";
+  const data = {
+    fullName : fullName,
+    email:email,
+    password:password,
+    phone:phone
+  }
+  return axios.post(URL_BACKEND,data);
+  
+}
+
 
 export{
   createUserAPI,updateUserAPI,deleteUserAPI,
-  fetchAllUserAPI,handleUploadFile,updateUserAvatarAPI
+  fetchAllUserAPI,handleUploadFile,updateUserAvatarAPI,registerUserAPI
 }
